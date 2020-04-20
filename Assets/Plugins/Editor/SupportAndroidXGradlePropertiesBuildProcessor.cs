68a2f60b2228edd2419b689ce4128c5abc87f05d
anjs <anjs@ushareit.com>
2020-04-20 22:52:46 +0800

    [UnityDemo]Add androidx enable property in gradle.properties file when generate project

---
+++ Assets/Plugins/Editor/SupportAndroidXGradlePropertiesBuildProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEditor.Android;
+using UnityEngine;
+
+public class SupportAndroidXGradlePropertiesBuildProcessor : IPostGenerateGradleAndroidProject
+{
+    public int callbackOrder => throw new System.NotImplementedException();
+
+    public void OnPostGenerateGradleAndroidProject(string path)
+    {
+        Debug.Log("Bulid path : " + path);
+        string gradlePropertiesFile = path + "/gradle.properties";
+        if (File.Exists(gradlePropertiesFile))
+        {
+            File.Delete(gradlePropertiesFile);
+        }
+        StreamWriter writer = File.CreateText(gradlePropertiesFile);
+        writer.WriteLine("org.gradle.jvmargs=-Xmx4096M");
+        writer.WriteLine("android.useAndroidX=true");
+        writer.WriteLine("android.enableJetifier=true");
+        writer.Flush();
+        writer.Close();
+    }
+}
